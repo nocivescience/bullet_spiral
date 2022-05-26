7cf4dd00764e673f16178ce1dd494db2391eb8b8
noceive <nocivescience@gmail.com>
2022-05-26 02:22:28 -0400

    proyecto terminado

--- bullet.py
+++ bullet.py
@@ -3,17 +3,17 @@ class Bullet(Triangle):
     CONFIG={
         'fill_opacity':1,
         'stroke_width':0,
-        'length':0.1,
+        'length':DEFAULT_ARROW_TIP_LENGTH,
         'start_angle':PI,
         'aspect':1.5,
     }
     def __init__(self, **kwargs):
-        Triangle.__init__(self, **kwargs)
-        self.stretch_to_fit_height(self.CONFIG['aspect'])
-        self.stretch_to_fit_width(.8)
+        super().__init__(**kwargs)
+        self.stretch_to_fit_height(self.CONFIG['length'])
+        self.stretch_to_fit_width(self.CONFIG['length']*self.CONFIG['aspect'])
         self.points[5:7]+=np.array([self.CONFIG['length'],0,0])
-        self.scale(0.4)
-    def get_angle2(self):
+        self.scale(0.5)
+    def get_angle(self):
         return angle_of_vector(self.get_vector())
     def get_vector(self):
         return self.point_from_proportion(0.5)-self.get_start()
@@ -23,27 +23,27 @@ class BulletScene(Scene):
         vg2=VGroup()
         self.an=0
         def update_bullet(obj,dt):
-            obj.add(Bullet()).scale(0.7).rotate(PI*dt+self.an*10)
+            obj.add(Bullet().scale(0.7).rotate(10+self.an*10))
             for k in obj:
-                k.shift(dt*20*np.array([
-                    np.cos(k.get_angle2()),
-                    np.sin(k.get_angle2()),
+                k.shift(dt*2*np.array([
+                    np.cos(k.get_angle()),
+                    np.sin(k.get_angle()),
                     0
                 ]))
                 if abs(np.linalg.norm(k.get_center()-ORIGIN))>7:
                     obj.remove(k)
-            self.an+=DEGREES
+            self.an+=1*DEGREES
         def update_bullet_2(obj,dt):
-            obj.add(Bullet()).scale(0.7).rotate(PI*dt*+self.an*10)
+            obj.add(Bullet().scale(0.7).rotate(PI+10+self.an*10))
             for k in obj:
-                k.shift(dt*20*np.array([
-                    np.cos(k.get_angle2()),
-                    np.sin(k.get_angle2()),
+                k.shift(dt*2*np.array([
+                    np.cos(k.get_angle()),
+                    np.sin(k.get_angle()),
                     0
                 ]))
                 if abs(np.linalg.norm(k.get_center()-ORIGIN))>7:
                     obj.remove(k)
-            self.an+=DEGREES
+            self.an+=1*DEGREES
         vg1.add_updater(update_bullet)
         vg2.add_updater(update_bullet_2)
         self.add(vg1,vg2)
