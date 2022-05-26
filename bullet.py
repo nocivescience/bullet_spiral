9d90367cd77926004e42dab70bd1ae2f7d903d67
noceive <nocivescience@gmail.com>
2022-05-26 01:37:20 -0400

    balas conseguidas del youtube

--- bullet.py
+++ bullet.py
@@ -1,50 +1,59 @@
 from manim import *
+
 class Bullet(Triangle):
-    CONFIG={
-        'fill_opacity':1,
-        'stroke_width':0,
-        'length':0.1,
-        'start_angle':PI,
-        'aspect':1.5,
+    CONFIG = {
+        "fill_opacity": 1,
+        "stroke_width": 0,
+        "length": DEFAULT_ARROW_TIP_LENGTH,
+        "start_angle": PI,
+        "aspect": 1.5,
     }
+
     def __init__(self, **kwargs):
-        Triangle.__init__(self, **kwargs)
-        self.stretch_to_fit_height(self.CONFIG['aspect'])
-        self.stretch_to_fit_width(.8)
-        self.points[5:7]+=np.array([self.CONFIG['length'],0,0])
-        self.scale(0.4)
-    def get_angle2(self):
+        super().__init__(**kwargs)
+
+        self.stretch_to_fit_height(self.CONFIG['length'])
+        self.stretch_to_fit_width(self.CONFIG['length'] * self.CONFIG['aspect'])
+        self.points[5:7] += np.array([self.CONFIG['length'], 0, 0])
+        self.scale(0.5)
+
+    def get_angle(self):
         return angle_of_vector(self.get_vector())
+
     def get_vector(self):
-        return self.point_from_proportion(0.5)-self.get_start()
-class BulletScene(Scene):
+        return self.point_from_proportion(0.5) - self.get_start()
+
+class TestBullet(Scene):
     def construct(self):
-        vg1=VGroup()
-        vg2=VGroup()
-        self.an=0
-        def update_bullet(obj,dt):
-            obj.add(Bullet()).scale(0.7).rotate(PI*dt+self.an*10)
+        vqueue1 = VGroup()
+        vqueue2 = VGroup()
+        self.an = 0
+
+        def update_bullet1(obj, dt):
+            obj.add(Bullet().scale(0.7).rotate( 10 + self.an * 10))
             for k in obj:
-                k.shift(dt*20*np.array([
-                    np.cos(k.get_angle2()),
-                    np.sin(k.get_angle2()),
+                k.shift(dt * 2 * np.array([
+                    np.cos(k.get_angle()),
+                    np.sin(k.get_angle()),
                     0
                 ]))
-                if abs(np.linalg.norm(k.get_center()-ORIGIN))>7:
+                if abs(np.linalg.norm(k.get_center() - ORIGIN)) > 7:
                     obj.remove(k)
-            self.an+=DEGREES
-        def update_bullet_2(obj,dt):
-            obj.add(Bullet()).scale(0.7).rotate(PI*dt*+self.an*10)
+            self.an += 1 * DEGREES
+
+        def update_bullet2(obj, dt):
+            obj.add(Bullet().scale(0.7).rotate(PI +  10 + self.an * 10))
             for k in obj:
-                k.shift(dt*20*np.array([
-                    np.cos(k.get_angle2()),
-                    np.sin(k.get_angle2()),
+                k.shift(dt * 2 * np.array([
+                    np.cos(k.get_angle()),
+                    np.sin(k.get_angle()),
                     0
                 ]))
-                if abs(np.linalg.norm(k.get_center()-ORIGIN))>7:
+                if abs(np.linalg.norm(k.get_center() - ORIGIN)) > 7:
                     obj.remove(k)
-            self.an+=DEGREES
-        vg1.add_updater(update_bullet)
-        vg2.add_updater(update_bullet_2)
-        self.add(vg1,vg2)
+            self.an += 1 * DEGREES
+
+        vqueue1.add_updater(update_bullet1)
+        vqueue2.add_updater(update_bullet2)
+        self.add(vqueue1,vqueue2)
         self.wait(1)
